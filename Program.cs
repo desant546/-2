d8eec4d1831474078d5468452cd94ea3d6c5f3be
desant546 <zombi_desant@mail.ru>
2023-03-16 19:24:55 +0700

    Test2
    Test2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 82;
+            long earthAge = 4540000000;
+            bool isSwims = false;
+            byte internetSpeed = 100;
+            sbyte tempIce = -28;
+            ushort theBombExplodesWhen = 0;
+            float pipieceOfCake = 3.15f;
+            double zombieAreAlive = 12547.5;
+            string name = "Денис";
+            char symbolI = 'Я';
+        }
+    }
+}
